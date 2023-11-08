dcc7f759434302e0f51271e751ebd04e65c172b7
Dante Gabriel Tokomoto Takatu <113074962+Dante-Gabriel-Tokomoto-Takatu@users.noreply.github.com>
2023-11-07 22:12:45 -0300

    Create index.php

---
+++ Dia 01/index.php
@@ -0,0 +1,22 @@
+<?php
+echo "Hello World!";
+
+//Meu primeiro código em php.
+//echo em php é igual printf em C.
+
+/*
+  Vários
+  Comentários
+  por
+  Linha 
+*/
+
+//Escrevendo minha primeira variável em php.
+
+$teste = "<br>Ola";
+echo $teste;
+echo $teste . "Mundo";
+
+//Para concatenar, basta usar ponto (.)
+
+
